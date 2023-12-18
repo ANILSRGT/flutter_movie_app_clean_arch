11c45633011f0a7e44050f814377ef2eced06e30
ANILSRGT <anilsorgit6060@outlook.com.tr>
2023-12-18 17:40:30 +0300

    Refactor lang.sh script to support custom source and output directories

--- scripts/lang.sh
+++ scripts/lang.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-flutter pub run easy_localization:generate -O lib/products/init/language -f keys -o locale_keys.g.dart --source-dir assets/translations
+if [ $# -ne 2 ] && [ $# -ne 0 ]
+then
+    echo "Usage 2: sh lang.sh <source-dir> <output-dir>"
+    echo "Usage with default variables: sh lang.sh"
+    exit 1
+fi
+
+if [ $# -eq 2 ]
+then
+    flutter pub run easy_localization:generate -O $2 -f keys -o locale_keys.g.dart --source-dir $1
+    exit 1
+fi
+
+flutter pub run easy_localization:generate -O lib/app/init/localization -f keys -o locale_keys.g.dart --source-dir assets/translations
